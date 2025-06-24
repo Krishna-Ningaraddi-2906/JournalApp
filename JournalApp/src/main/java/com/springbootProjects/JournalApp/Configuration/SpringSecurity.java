0d241ee394ac7fcb93b07abe0f3f88f1414b4ab5
Krishna-Ningaraddi-2906 <krishna.ningaraddi08@gmail.com>
2025-06-24 16:23:41 +0530

    Spring Security config

---
+++ JournalApp/src/main/java/com/springbootProjects/JournalApp/Configuration/SpringSecurity.java
@@ -0,0 +1,19 @@
+package com.springbootProjects.JournalApp.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity extends WebSecurityConfigurerAdapter
+{
+  protected void configure(HttpSecurity http) throws Exception
+  {
+      http.authorizeHttpRequests().antMatchers("/hello").
+              permitAll().anyRequest().
+              authenticated().and().
+              formLogin();
+  }
+}
